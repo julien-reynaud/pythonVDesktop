eff9581e4064505cf4a1a396414b2af1c3617af8
Leo Herber <leo.herber@hotmail.com>
2025-03-03 16:16:18 +0100

    adding game to desktop

--- pythonVDesktop/jeu/urls.py
+++ pythonVDesktop/jeu/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'jeu'
+
 urlpatterns = [
     path('', views.affiche_jeu, name='affiche_jeu'),
     path('get-scores/', views.get_scores, name='get_scores'),
